eda8b724684045ff1efe84c9f1f2c8bdd790456c
Jobayer_ak <jobayer.ak@gmail.com>
2022-09-02 20:48:46 +0600

    all user api is done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dbConnect = require("./utils/dbConnect");
 require("dotenv").configure;
+const userInfo = require("./data.json");
 
 const app = express();
 const PORT = 5000;
@@ -12,6 +13,13 @@ app.use(express.json());
 
 // database connection
 dbConnect();
+
+// Api
+// get all user api
+app.use("/", (req, res) => {
+  res.send(userInfo);
+  //   res.send("get all user data");
+});
 
 // not found
 app.all("*", (req, res) => {
